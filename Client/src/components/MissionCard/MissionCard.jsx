569b2532fa3b14bfeefffc32520db9b1abfe65fc
Hala Alchammah <hala.alchammah@gmail.com>
2024-11-24 15:27:23 -0500

    add mission cards on home page, connect server to client, extract JSON data

--- Client/src/components/MissionCard/MissionCard.jsx
+++ Client/src/components/MissionCard/MissionCard.jsx
@@ -1,48 +1,31 @@
 import "./MissionCard.scss";
-import backImage from "../../assets/Icons/arrow_back-24px.svg";
+// import backImage from "../../assets/Icons/arrow_back-24px.svg";
 import { useNavigate } from "react-router-dom";
 
-function MissionCard({
-  title,
-  returnPath,
-  handleWarehouseEditOnClick,
-  headerContent,
-  children,
-}) {
+function MissionCard({ mission }) {
   const navigate = useNavigate();
 
   return (
     <div className="card">
       <div className="card__header">
         <div className="card__title-bar">
-          {returnPath && (
-            <img
-              className="card__back_image"
-              src={backImage}
-              onClick={() => {
-                navigate("/");
-              }}
-            />
-          )}
-          <h1 className="card__name">{title}</h1>
-          {handleWarehouseEditOnClick && (
-            <button
-              className="card__edit-button card__edit-button--mob"
-              onClick={handleWarehouseEditOnClick}
-            ></button>
-          )}
+          <img
+            className="card__back_image"
+            src=""
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <h1 className="card__name">{mission.title}</h1>
         </div>
         <div className="card__header-section">
-          {headerContent}
-          {handleWarehouseEditOnClick && (
-            <button
-              className="card__edit-button card__edit-button--tablet"
-              onClick={handleWarehouseEditOnClick}
-            ></button>
-          )}
+          <p>Launch Date: {mission.launch}</p>
         </div>
       </div>
-      <div className="card__content">{children}</div>
+      <div className="card__content">
+        <img src={mission.image} alt={`${mission.title} patch`} />
+        {/* Add more mission details here as needed */}
+      </div>
     </div>
   );
 }
