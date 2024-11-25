39429f2c6d73f06d873b8120a8a2346863814453
Hala Alchammah <hala.alchammah@gmail.com>
2024-11-24 19:24:06 -0500

    initial SCSS styling for the home page, mission cards

--- Client/src/components/MissionCard/MissionCard.jsx
+++ Client/src/components/MissionCard/MissionCard.jsx
@@ -7,28 +7,31 @@ function MissionCard({ mission }) {
   const BASE_URL = import.meta.env.VITE_API_URL;
 
   return (
-    <div className="card">
-      <div className="card__header">
-        <div className="card__title-bar">
-          {/* <img
+    <div className="cards-container">
+      <div className="card">
+        <div className="card__header">
+          <div className="card__title-bar">
+            {/* <img
             className="card__back_image"
             src=""
             onClick={() => {
               navigate("/");
             }} */}
-          {/* /> */}
-          <h1 className="card__name">{mission.title}</h1>
+            {/* /> */}
+            <h1 className="card__name">{mission.title}</h1>
+          </div>
+          <div className="card__header-section">
+            <p>Launch Date: {mission.launch}</p>
+          </div>
         </div>
-        <div className="card__header-section">
-          <p>Launch Date: {mission.launch}</p>
+        <div className="card__content">
+          <img
+            className="card__content--patch"
+            src={`${BASE_URL}${mission.image}`}
+            alt={`${mission.title} patch`}
+          />
+          {/* Add more mission details here as needed */}
         </div>
-      </div>
-      <div className="card__content">
-        <img
-          src={`${BASE_URL}${mission.image}`}
-          alt={`${mission.title} patch`}
-        />
-        {/* Add more mission details here as needed */}
       </div>
     </div>
   );
--- Server/index.js
+++ Server/index.js
@@ -25,6 +25,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
 import missionRoutes from "./routes/missions.js";
 
 // Basic Route
