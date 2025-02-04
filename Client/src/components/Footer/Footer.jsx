421dcb4835ab61a75f415c8448e94f493f7f0f76
Hala Alchammah <hala.alchammah@gmail.com>
2025-02-04 11:27:56 -0500

    remove text from footer

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
     <footer>
       <div className="footer__text">
         <di className="footer__text--credits">© Canadian Space Agency</di>
-        <div className="footer__text--light">
+        {/* <div className="footer__text--light">
           L e t y o u r f r e a k f l a g f l y
-        </div>
+        </div> */}
       </div>
     </footer>
   );
